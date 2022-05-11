1d8d91de1f0de01b94794c2a5811a37f4cf83341
alvine <alvine@abdis-MacBook-Pro.local>
2022-05-11 08:38:57 +0700

    update home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
