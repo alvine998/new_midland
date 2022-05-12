b4a4861e6d7ce9e433267a1779b08e68788a89fe
alvinecom2018@gmail.com <alvinecom2018@gmail.com>
2022-05-12 14:56:32 +0700

    profile pages progress

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import logo from './images/logo2.png';
+import company from './images/company_banner.png';
 
 export {
-    logo
+    logo,
+    company
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import { company, logo } from '../../assets';
+import Footer from '../../components/Footer';
 import Sidebar from '../../components/Sidebar';
+import './style.css'
 
 const Profile = () => {
-    return ( 
-        <div style={{overflow:"hidden"}}>
+    return (
+        <div style={{ overflow: "hidden" }}>
             <div className='row'>
                 <div className='col-md-2'>
-                    <Sidebar profile/>
+                    <Sidebar profile />
                 </div>
                 <div className='col-md'>
-                    <p>
-                        Ini Profile
-                    </p>
+                    <img src={company} className="banner-profile" />
+                    <div style={{ paddingTop: 20 }}>
+                        <h2 style={{ textAlign: "center" }}>Tentang Midland Properti</h2>
+                        <div>
+                            <div className='row'>
+                                <div className='col-md'>
+                                    <h5 className='description-profile'>
+                                        PT. Midland Kreator Properti adalah sebuah perusahaan yang bergerak
+                                        di bidang developer properti khususnya real estate dengan mengutamakan kualitas,
+                                        ketepatan waktu, dan harga yang terjangkau dengan sistem dan konsep syariah. Kami 
+                                        hadir sebagai developer perumahan terjangkau dan berkomitmen dalam memberikan 
+                                        pelayanan untuk rumah idaman anda.
+                                    </h5>
+                                </div>
+                                <div className='col-md-4'>
+                                    <img src={logo} className="img-profile"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ paddingTop: 50 }}>
+                        <Footer />
+                    </div>
                 </div>
             </div>
         </div>
-     );
+    );
 }
 
 export default Profile;
