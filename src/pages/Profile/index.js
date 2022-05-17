69fb82ac28cb2156c14d77a02e69748eb6530c56
Staff SIESTA <staff@siesta.id>
2022-05-17 13:04:09 +0700

    final profile pages

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { company, logo } from '../../assets';
+import { company, logo, misi, visi } from '../../assets';
 import Footer from '../../components/Footer';
 import Sidebar from '../../components/Sidebar';
 import './style.css'
@@ -21,13 +21,44 @@ const Profile = () => {
                                     <h5 className='description-profile'>
                                         PT. Midland Kreator Properti adalah sebuah perusahaan yang bergerak
                                         di bidang developer properti khususnya real estate dengan mengutamakan kualitas,
-                                        ketepatan waktu, dan harga yang terjangkau dengan sistem dan konsep syariah. Kami 
-                                        hadir sebagai developer perumahan terjangkau dan berkomitmen dalam memberikan 
+                                        ketepatan waktu, dan harga yang terjangkau dengan sistem dan konsep syariah. Kami
+                                        hadir sebagai developer perumahan terjangkau dan berkomitmen dalam memberikan
                                         pelayanan untuk rumah idaman anda.
                                     </h5>
                                 </div>
                                 <div className='col-md-4'>
-                                    <img src={logo} className="img-profile"/>
+                                    <img src={logo} className="img-profile" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr/>
+                        <div style={{paddingTop:50}}>
+                            <div className='row'>
+                                <div className='col-md-4'>
+                                    <img src={visi} className="img-visi-profile" />
+                                </div>
+                                <div className='col-md'>
+                                    <h5 className='description-profile'>
+                                        Menjadi salah satu dari 10 pengembang terbaik dan terpercaya di Indonesia
+                                        dengan menerapkan konsep syariah, serta mengedepankan kualitas, inovasi, dan 
+                                        ketepatan waktu demi memberikan pelayanan maksimal baik bagi investor maupun 
+                                        konsumen
+                                    </h5>
+                                </div>
+                            </div>
+                            <div className='row' style={{paddingTop:20}}>
+                                <div className='col-md'>
+                                    <h5 className='description-profile'>
+                                        - Menghadirkan properti dengan harga terjangkau dan berkualitas <br/>
+                                        - Menjadi solusi dalam mengatasi masalah sulitnya masyarakat dalam memiliki
+                                        hunian <br/>
+                                        - Mengedepankan nilai amanah dan jujur dalam memberikan pelayanan kepada
+                                        investor dan konsumen
+                                    </h5>
+                                </div>
+                                <div className='col-md-4'>
+                                    <img src={misi} className="img-visi-profile" />
                                 </div>
                             </div>
                         </div>
